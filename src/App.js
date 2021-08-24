9d538f99c4ab43717e71e32deaefbc7b7ed12d10
inshiya-technoark <incya21@gmail.com>
2021-08-24 23:24:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { Link } from 'react-router-dom';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './Pages/Footer';
-
+import SellerDashboard from './Pages/SellerDashboard/MyAccount';
+import Profile from './Pages/SellerDashboard/Profile';
 
 function App() {
   return (
     <Router>
-      
+      {
+        <Switch>
+         <Route path='/' exact component={SellerDashboard} />
+         <Route path='/sellerhub' exact component={SellerDashboard} />
+         <Route path='/profile' component={Profile} />
+         </Switch>
+      }
      
       {
          <Footer />
