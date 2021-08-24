869002cbad9d7f064571bafcaa76739b710c3516
inshiya-technoark <incya21@gmail.com>
2021-08-24 23:41:19 +0530

    commit pls

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './Pages/Footer';
 import SellerDashboard from './Pages/SellerDashboard/MyAccount';
 import Profile from './Pages/SellerDashboard/Profile';
+import Payments from './Pages/Payments';
+import Overview from './Pages/Overview';
+import Messages from './Pages/Messages';
+import Orders from './Pages/Orders';
+import Quotes from './Pages/Quotes';
 
 function App() {
   return (
     <Router>
-    
-      {
-        <Switch>
+      
+      <Switch>
          <Route path='/' exact component={SellerDashboard} />
-         <Route path='/sellerhub' component={SellerDashboard} />
+         <Route path='/sellerhub' exact component={SellerDashboard} />
          <Route path='/profile' component={Profile} />
-         </Switch>
-      }
-     
-      {
+         <Route path='/payments' component={Payments} />
+         <Route path='/overview' component={Overview} />
+         <Route path='/messages' component={Messages} />
+         <Route path='/order' component={Orders} />
+         <Route path='/quotes' component={Quotes} />
+       </Switch> 
+    
+      
          <Footer />
-      }
+      
     </Router>
   );
 }
