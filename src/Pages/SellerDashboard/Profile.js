869a4f5f00482b334ea41fbba8fbaef2e6126128
inshiya-technoark <incya21@gmail.com>
2021-09-03 22:06:08 +0530

    commmit pls pt.2

--- src/Pages/SellerDashboard/Profile.js
+++ src/Pages/SellerDashboard/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import styled from "styled-components";
